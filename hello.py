c1014299478d36466f882b252109f433d56b4f0c
hoserg <hoserg@mail.ru>
2016-11-17 15:44:48 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    body = 'Hello, world!'
-    start_response(staus, headers)
-    return [body]
+	raw_uri = str(environ.get('RAW_URI'))
+	raw_uri = raw_uri[2:]
+	params = raw_uri.split('&')
+	
+	data = ''
+	for param in params:
+		data += param + '\r\n'
+	
+	start_response("200 OK", [
+	  ("Content-Type", "text/plain"),
+	  ("Content-Length", str(len(data)))
+	])
+	return iter([data])
