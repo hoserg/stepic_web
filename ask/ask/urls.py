8bfd54efb05385a41d2939838348e214a90c306e
hoserg <hoserg@mail.ru>
2016-11-21 16:16:58 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', 'views.proba'),
     url(r'^login/$', 'views.proba'),
     url(r'^signup/$', 'views.proba'),
-    url(r'^question/(\d+)$', 'views.proba'),
+    url(r'^question/(\d+)$', import(qa.urls)),
     url(r'^ask/$', 'views.proba'),
     url(r'^popular/$', 'views.proba'),
     url(r'^new/$', 'views.proba'),
