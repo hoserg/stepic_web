d82310485f3a4e816a8c7f9f2330a87408577acc
hoserg <hoserg@mail.ru>
2016-11-21 18:52:43 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from ask import views
-from django.ask import qa
+#from django.ask import qa
 
 #from django.contrib import admin
 #admin.autodiscover()
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^$', 'views.proba'),
     url(r'^login/$', 'views.proba'),
     url(r'^signup/$', 'views.proba'),
-    url(r'^question/', 'qa.views.test'), #include(qa.urls)),
+    url(r'^question/', include(qa.urls)),
     url(r'^ask/$', 'views.proba'),
     url(r'^popular/$', 'views.proba'),
     url(r'^new/$', 'views.proba'),
