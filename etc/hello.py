8f8dac5d913ff80421111121b2db2957fec6b7d1
hoserg <hoserg@mail.ru>
2016-11-21 14:31:51 +0200

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,2 @@
-CONFIG = {
-  'mode': 'wsgi',
-  'working_dir': '/home/box/web',
-  'python': '/usr/bin/python',
-  'args': (
-    '--bind=0.0.0.0:8080',
-    '--workers=16',
-    '--timeout=60',
-    'hello:app',
-  ),
-}
+pythonpath = '/home/box/web'
+bind = "0.0.0.0:8080"
