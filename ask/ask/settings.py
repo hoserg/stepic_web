898a3ab48b3e25c3baf20075581aa013024fdc6b
hoserg <hoserg@mail.ru>
2016-11-21 16:02:09 +0200

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '37679ct3^^&%h&l1vndmji4glp%a!i&rpdg$q^5%a*bh6o+wq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
