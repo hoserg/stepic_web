5f125ec9cc8c6e9bb5402b54a71987397d3e8407
hoserg <hoserg@mail.ru>
2016-11-21 19:00:11 +0200

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,6 +1,6 @@
 rom django.conf.urls import patterns, include, url
 from . import views
 
-urlpatterns = patterns('',
-    url(r'^/(\d+)/$', 'views.test'),
-)
+urlpatterns = [
+    url(r'^(\d+)/$', 'views.test'),
+]
