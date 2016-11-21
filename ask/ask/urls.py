7c97012116891188de7d065332d34e0ae818cf55
hoserg <hoserg@mail.ru>
2016-11-21 18:43:54 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^$', 'views.proba'),
     url(r'^login/$', 'views.proba'),
     url(r'^signup/$', 'views.proba'),
-    url(r'^question/(\d+)/$', 'qav.test'), #include(qa.urls)),
+    url(r'^question/', 'qa.views.test'), #include(qa.urls)),
     url(r'^ask/$', 'views.proba'),
     url(r'^popular/$', 'views.proba'),
     url(r'^new/$', 'views.proba'),
