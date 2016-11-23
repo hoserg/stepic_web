f078f134a51c466fadd18c4ee2d37a16c8508fd1
hoserg <hoserg@mail.ru>
2016-11-23 14:37:06 +0200

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-def question(request, *args, **kwargs):
-    return HttpResponse('OK')
+def question(request, qid):
+    qmain = Question.objects.get(id=qid)
+    qanswer = Answer.objects.filter(question__id=qid)
+    t = loader.get_template("question.htm")
+    c = Context({'question':qmain, 'answer':qanswer, 'request':request})
+    return HttpResponse(t.render(c))
