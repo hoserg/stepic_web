d784da1b96e0db2be3ad2ef6a316a048b44b2c79
hoserg <hoserg@mail.ru>
2016-11-23 15:27:39 +0200

    Update views.py
    2.5. Failed test #2. 'Question' object has no attribute 'answer_set'

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from qa.models import Question
 
 def proba(request):
     return HttpResponse('OK')
 
 def newqa(request):
-    qmain = qa.Question.objects.all().order_by('-id')
+    qmain = Question.objects.all().order_by('-id')
     t = loader.get_template("new.html")
     c = Context({'questions':qmain, 'request':request})
     return HttpResponse(t.render(c))
 
 def popular(request):
-    qmain = qa.Question.objects.all().order_by('-rating')
+    qmain = Question.objects.all().order_by('-rating')
     t = loader.get_template("popular.html")
     c = Context({'questions':qmain, 'request':request})
     return HttpResponse(t.render(c))
