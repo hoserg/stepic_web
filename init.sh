effa9fbd04afc9f2c019ee5abc267737befb8264
hoserg <hoserg@mail.ru>
2016-11-23 09:46:58 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE DATABASE dbstep CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -uroot -e "CREATE USER 'hoserg'@'localhost' IDENTIFIED BY 'hoserg';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'hoserg'@'localhost';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
@@ -6,4 +12,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
 
-sudo /etc/init.d/mysql start
+python /home/box/web/ask/manage.py syncdb
