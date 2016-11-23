4c5660a06c56243d4727c92ae4b3510d0494ca4b
hoserg <hoserg@mail.ru>
2016-11-23 18:15:25 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-from ask import views
+#from ask import views
 #from ask.qa.views import *
 
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('qa.urls')),
 ]
