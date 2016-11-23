68afb73793cfa9b8c63b6f6548589b0dd82345ff
hoserg <hoserg@mail.ru>
2016-11-23 13:25:39 +0200

    Update models.py

--- ask/ask/models.py
+++ ask/ask/models.py
@@ -4,16 +4,16 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=64)
     text = models.TextField()
-    added_at = models.DateField(auto_now=False, auto_now_add=False)
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default = 0)
     author = models.ForeignKey(User)  
-    likes = models.IntegerField(default=0)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
     def __unicode__(self):
         return self.title
 
 class Answer(models.Model):
     text = models.CharField(max_length=255)
-    added_at = models.DateField(auto_now=False, auto_now_add=False)
-    question = models.ForeignKey(Question)  
-    author = models.ForeignKey(User)
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, related_name='answer_question')  
+    author = models.ForeignKey(User, related_name='answer_user')
