7395644b8cf11f51dfbd091e945b9b8e5b94f44e
hoserg <hoserg@mail.ru>
2016-11-23 13:01:12 +0200

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -59,9 +59,13 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'step_base',
+        'USER': 'hoserg',
+        'PASSWORD': 'hhh',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        }
 }
 
 # Internationalization
