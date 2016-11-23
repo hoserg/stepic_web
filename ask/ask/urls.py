f78e74a172e236ed62b6d6f60d8bcdf954d77d25
hoserg <hoserg@mail.ru>
2016-11-23 15:59:32 +0200

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from ask import views
-from qa.views import *
+from ask.qa.views import *
 
 #from django.contrib import admin
 #admin.autodiscover()
