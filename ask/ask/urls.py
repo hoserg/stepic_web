52127d512c89817396e4ed93e56032e19243e303
hoserg <hoserg@mail.ru>
2016-11-23 16:03:15 +0200

    Update urls.py
    2.5. все перенаправив на qa

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls import patterns, include, url
 from ask import views
-from ask.qa.views import *
+#from ask.qa.views import *
 
 #from django.contrib import admin
 #admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', 'newqa'),
-    url(r'^login/$', 'views.proba'),
-    url(r'^signup/$', 'views.proba'),
-    url(r'^question/', include('qa.urls')),
-    url(r'^ask/$', 'views.proba'),
-    url(r'^popular/$', 'popular'),
-    url(r'^new/$', 'views.proba'),
+    url(r'^', include('qa.urls')),
 ]
