c7c18cf189649c1dca893ce7be086fed291551c3
hoserg <hoserg@mail.ru>
2016-11-23 14:40:37 +0200

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -5,5 +5,5 @@ def question(request, qid):
     qmain = Question.objects.get(id=qid)
     qanswer = Answer.objects.filter(question__id=qid)
     t = loader.get_template("question.htm")
-    c = Context({'question':qmain, 'answer':qanswer, 'request':request})
+    c = Context({'question':qmain, 'answers':qanswer, 'request':request})
     return HttpResponse(t.render(c))
