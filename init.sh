8c76953e6d1c225490ec51a03a8d5f24d5598d8c
hoserg <hoserg@mail.ru>
2016-11-23 09:21:31 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 #gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-#gunicorn -c /home/box/web/etc/ask.py wsgi --daemon
 sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
