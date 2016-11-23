944e7c00e78dd0a64c763f8037d6a7e92bbf2518
hoserg <hoserg@mail.ru>
2016-11-23 15:31:55 +0200

    Update views.py

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from qa.models import Question
 
 def proba(request):
     return HttpResponse('OK')
 
-def newqa(request):
-    qmain = Question.objects.all().order_by('-id')
-    t = loader.get_template("new.html")
-    c = Context({'questions':qmain, 'request':request})
-    return HttpResponse(t.render(c))
-
-def popular(request):
-    qmain = Question.objects.all().order_by('-rating')
-    t = loader.get_template("popular.html")
-    c = Context({'questions':qmain, 'request':request})
-    return HttpResponse(t.render(c))
