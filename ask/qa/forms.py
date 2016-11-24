004068a9895f5b6d777ff182b956335e0058790d
hoserg <hoserg@mail.ru>
2016-11-24 15:52:54 +0200

    field -> fields

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -5,9 +5,9 @@ from models import Question, Answer
 class AskForm(forms.ModelForm):
     class Meta:
         model = Question
-        field = ('title', 'text')
+        fields = ('title', 'text')
         
 class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
-        field = ('text', 'question')
+        fields = ('text', 'question')
